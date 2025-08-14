aa0fdb732c376533507d38d93bfdcc640590752e
JoãoVitor <joaovitor01990@gmail.com>
2025-08-14 09:41:15 -0300

    Update caixaEletronico.c

--- 2bimestral/caixaEletronico.c
+++ 2bimestral/caixaEletronico.c
@@ -7,18 +7,18 @@ int main() {
     
     printf(" Depósito Bancário \n");
     printf("Digite sua idade: ");
-    if (scanf("%hd", &idade) != 1 || idade < 0) {
-        printf("Entrada inválida para idade!\n");
+    if (scanf("%hd", &idade) != 1 || idade < 18) {
+        printf("Voce nao pode fazer um deposito\n");
         return 1;
     }
     printf("Digite o número da conta: ");
     if (scanf("%d", &conta) != 1 || conta <= 0) {
-        printf("Entrada inválida para número da conta!\n");
+        printf("Conta nao encontrada\n");
         return 1;
     }
     printf("Digite o montante a depositar: ");
     if (scanf("%ld", &montante) != 1 || montante <= 0) {
-        printf("Entrada inválida para montante!\n");
+        printf("montante nao disponivel\n");
         return 1;
     }
     printf("Idade: %hd anos\n", idade);
